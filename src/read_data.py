14de1632daad1d7f59c7fdc96b4749e3e7222f92
mahapsub <smahapat@u.northwestern.edu>
2019-04-12 19:20:44 -0500

    basic structur

---
+++ src/read_data.py
@@ -0,0 +1,20 @@
+import numpy 
+import torch
+
+
+
+class corpus():
+    def __init__():
+        pass
+
+    def process():
+        pass
+
+
+def main():
+    print('welcome rhett!!')
+
+
+
+if if __name__ == "__main__":
+    main()
