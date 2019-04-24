4c79c138201459d9e3181a4b625bbe7063a8cf8b
mahapsub <smahapat@u.northwestern.edu>
2019-04-23 20:22:27 -0500

    tensorflow init archit

--- src/read_data.py
+++ src/read_data.py
@@ -1,17 +1,26 @@
 import numpy as np
 import torch
+import tensorflow as tf
 import glob
 import os 
 from collections import Counter
 
 
 
-dir_path = 'data/brown/'
+# dir_path = 'data/corpora/'
 
+train_path = 'data/corpora/wiki.train.txt'
+valid_path = 'data/corpora/wiki.valid.txt'
+test_path = 'data/corpora/wiki.test.txt'
 
 class Corpus():
     def __init__(self):
-        self.process() # makes self.all_words_in_corpora, self.word_freq_table, self.vocabulary_length
+        self.process() 
+            # self.all_words_in_corpora
+            # self.word_freq_table
+            # self.vocabulary_length
+            # self.validation_data
+            # self.test_data
         self.num_features = 60
         self.window_length = 4
         self.batch_size = 128
@@ -20,7 +29,7 @@ class Corpus():
         self.curr_batch = 0
         self.num_of_possible_batches = self.get_total_batches()
 
-        print('creating corpus from file')
+        # print('creating corpus from file')
         
         self.index_mapper = dict()
         count = 0
@@ -28,8 +37,13 @@ class Corpus():
             self.index_mapper[word] = count
             count += 1
         self.C = self.createC()
-        print('Finished creating corpora representation from file')
+        self.inv_map = {v: k for k, v in self.index_mapper.items()}
+        # print('Finished creating corpora representation from file')
         # print(num_total_words)
+        print("Size of: ")
+        print('\t--training-set: \t\t{}'.format(len(self.all_words_in_corpora)))
+        print('\t--validation-set: \t\t{}'.format(len(self.validation_data)))
+        print('\t--test-set: \t\t\t{}'.format(len(self.test_data)))
     
     def get_batch(self):
         x= []
@@ -37,13 +51,14 @@ class Corpus():
         if self.curr_batch != self.num_of_possible_batches:
             curr_dataset = self.all_words_in_corpora[self.curr_batch*self.batch_size:(self.curr_batch+1)*self.batch_size]
             print('cur_dataset length: {}'.format(len(curr_dataset)))
-            print(curr_dataset)
             for i in range(len(curr_dataset)-self.window_length):
                 x_lst = curr_dataset[i:i+self.window_length]
-                x.append([self.get_index(word) for word in x_lst])
-                y.append(self.get_index(curr_dataset[i+self.window_length]))
+                # print('x:{}'.format(x_lst))
+                x.append([self.get_index_from_word(word) for word in x_lst])
+                y.append(self.get_index_from_word(curr_dataset[i+self.window_length]))
+                # print('y:{}'.format(curr_dataset[i+self.window_length]))
         self.curr_batch += 1
-        return x,y
+        return np.array(x),np.array(y).reshape(-1,1)
             
 
     def get_total_batches(self):
@@ -52,49 +67,83 @@ class Corpus():
         return np.random.rand(self.vocabulary_length, self.num_features)
             
     def process(self):
-        corpora_list = os.listdir(dir_path)
-        word_freq_table = Counter()
-        word_freq_table['<unk>'] = 0
         all_words = []
-        for file in corpora_list:
-            if file not in ['CONTENTS', 'README', 'cats.txt', 'categories.pickle']:
-                path_to_file = dir_path + file
-                with open(path_to_file, 'r') as f:
-                    lines = f.read().strip()
-                    all_lines = lines.split(' ')
-                    for word in all_lines:
-                        cleaned_word = word.split('/')[0]
-                        word_freq_table[cleaned_word] +=1
-                        all_words.append(cleaned_word)
+        word_freq_table = Counter()
+        with open(train_path, 'r') as f:
+            lines = f.read().strip()
+            all_lines = lines.split(' ')
+            for word in all_lines:
+                word_freq_table[word] +=1
+                all_words.append(word)
         self.all_words_in_corpora = all_words
         self.vocabulary_length = len(word_freq_table)
         self.word_freq_table = word_freq_table
-
+        with open(valid_path, 'r') as f:
+            lines = f.read().strip()
+            self.validation_data =  lines.split(' ')
+        with open(test_path, 'r') as f:
+            lines = f.read().strip()
+            self.test_data = lines.split(' ')
     def get_word_freq(self):
         return self.word_freq_table
     def get_all_words_from_corpora(self):
         return self.all_words_in_corpora
-    def get_index(self, word):
+    def get_index_from_word(self, word):
         if word in self.index_mapper:
             return self.index_mapper[word]
         else:
             return self.index_mapper['<unk>']
+    def get_word_from_index(self, index):
+        return self.inv_map[index]
     def get_probability(self, word):
         return self.word_freq_table[word]/self.vocabulary_length
     def get_feature_vector(self, idx):
         return self.C[int(idx), :]
 
 
-def main():    
+def tensorflow_implementation():
     corp = Corpus()
-    # print(corp.get_index('the'))
-    # print(len(corp.get_feature_vector('the')))
     X, Y = corp.get_batch()
-    print(X)
-    print()
-    print(Y)
-    print(len(X))
-    print(len(Y))
+    print(X.shape)
+    print(Y.shape)
+    
+    C = tf.Variable(corp.createC())
+
+    x = tf.placeholder(tf.float32, [None, corp.num_features])
+    y_pred = tf.placeholder(tf.float32, [None, corp.vocabulary_length])
+    y_actual = tf.placeholder(tf.float32, [None, Y.shape[1]])
+    
+    
+
+    # Create new weights and biases.
+    weights = tf.Variable(tf.zeros([corp.num_features, corp.vocabulary_length]))
+    biases = tf.Variable(tf.zeros([corp.vocabulary_length]))
+
+    # Calculate the layer as the matrix multiplication of
+    # the input and weights, and then add the bias-values.
+    layer_tanh = tf.nn.tanh(tf.matmul(x, weights) + biases)
+    y_pred = tf.nn.softmax(layer_tanh)
+    y_actual = tf.argmax(y_pred, axis=1)
+
+
+    
+
+    print(x)
+    print(y_pred)
+    print(y_actual)
+    print(C)
+
+    # C = tf.variable()
+
+
+
+def main():    
+    tensorflow_implementation()
+
+
+
+
+
 
 
 
