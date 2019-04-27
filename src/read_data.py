3f47624b5ae129c92758fbd65f541319a2731701
rhettdsouza13 <rhettdsouza13@gmail.com>
2019-04-27 03:16:14 -0500

    Reloading of models and Perplexity Cacl

--- src/read_data.py
+++ src/read_data.py
@@ -2,20 +2,20 @@ import numpy as np
 # import torch
 import tensorflow as tf
 import glob
-import os 
+import os
 from collections import Counter
 
 
 
 # dir_path = 'data/corpora/'
 
-train_path = 'data/corpora/wiki.train.txt'
-valid_path = 'data/corpora/wiki.valid.txt'
-test_path = 'data/corpora/wiki.test.txt'
+train_path = '../data/corpora/wiki.train.txt'
+valid_path = '../data/corpora/wiki.valid.txt'
+test_path = '../data/corpora/wiki.test.txt'
 
 class Corpus():
     def __init__(self):
-        self.process() 
+        self.process()
             # self.all_words_in_corpora
             # self.word_freq_table
             # self.vocabulary_length
@@ -30,7 +30,7 @@ class Corpus():
         self.num_of_possible_batches = self.get_total_batches()
 
         # print('creating corpus from file')
-        
+
         self.index_mapper = dict()
         count = 0
         for word, freq in self.word_freq_table.items():
@@ -44,7 +44,7 @@ class Corpus():
         print('\t--training-set: \t\t{}'.format(len(self.all_words_in_corpora)))
         print('\t--validation-set: \t\t{}'.format(len(self.validation_data)))
         print('\t--test-set: \t\t\t{}'.format(len(self.test_data)))
-    
+
     def get_batch(self):
         x= []
         y= []
@@ -61,13 +61,13 @@ class Corpus():
         if self.curr_batch == self.num_of_possible_batches:
             self.curr_batch = 0
         return np.array(x),np.array(y).reshape(-1,1)
-            
+
 
     def get_total_batches(self):
         return int(len(self.all_words_in_corpora)/self.batch_size)
     def createC(self):
         return np.random.rand(self.vocabulary_length, self.num_features)
-            
+
     def process(self):
         all_words = []
         word_freq_table = Counter()
@@ -107,10 +107,10 @@ def tensorflow_implementation():
     corp = Corpus()
     x_indexes = tf.placeholder(tf.int64, [None, corp.window_length], name='x_indexes')
     C = tf.Variable(corp.createC(), name='C')
-    X = tf.reshape(tf.nn.embedding_lookup(params=C,ids=x_indexes), [-1, corp.num_features*corp.window_length])
+    X = tf.reshape(tf.nn.embedding_lookup(params=C,ids=x_indexes), [-1, corp.num_features*corp.window_length], name='elookup')
     y_actual = tf.placeholder(tf.float32, [None, 1], name='y_actual')
-    
-    
+
+
     # Create new weights and biases.
     weights = tf.Variable(tf.truncated_normal([corp.num_features*corp.window_length, corp.vocabulary_length], dtype=tf.float64), name='w1')
     biases = tf.Variable(tf.truncated_normal([corp.vocabulary_length], dtype=tf.float64), name='bias')
@@ -118,18 +118,20 @@ def tensorflow_implementation():
     # Calculate the layer as the matrix multiplication of
     # the input and weights, and then add the bias-values.
 
-    
+
     layer_tanh = tf.nn.tanh(tf.matmul(X, weights) + biases)
     y_pred_prob = tf.nn.softmax(layer_tanh)
     y_pred = tf.cast(tf.argmax(y_pred_prob, axis=1),tf.float32, name='y_pred')
 
     cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=layer_tanh,
                                                            labels=y_actual)
-    cost = tf.reduce_mean(cross_entropy)
+    cost = tf.reduce_mean(cross_entropy, name='cost')
     tf.summary.scalar("loss", cost)
+    perplex = tf.exp(cost, name='perplexity')
+    tf.summary.scalar("perplexity", perplex)
     optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
     correct_prediction = tf.equal(y_pred, y_actual)
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accuracy')
     tf.summary.scalar("Accuracy", accuracy)
     merged_summary_op = tf.summary.merge_all()
     # session = tf.Session()
@@ -137,7 +139,7 @@ def tensorflow_implementation():
     logs_path = 'logs/'
     saver = tf.train.Saver()
     log_arr = []
-    
+
     with tf.Session() as session:
         session.run(tf.global_variables_initializer())
         summary_writer = tf.summary.FileWriter(logs_path, graph = tf.get_default_graph())
@@ -152,22 +154,41 @@ def tensorflow_implementation():
                 }
                 if val % 100 == 0:
                     print('batch num: {}'.format(val))
-                _,cst, acc, summary = session.run([optimizer,cost,accuracy, merged_summary_op], feed_dict=feed_dict_train)
+                _,cst, acc, summary= session.run([optimizer,cost,accuracy, merged_summary_op], feed_dict=feed_dict_train)
                 cur_batch = i*corp.get_total_batches()+val
                 summary_writer.add_summary(summary,cur_batch)
                 avg_cost += cst / corp.get_total_batches()
                 avg_acc += acc / corp.get_total_batches()
             if i%10==0:
                 saver.save(session, './model_chkpnts_{}/bengio_run_test'.format(str(i)), global_step=i)
-            
+
             print('epoch {0} ---- acc:{1}, cost:{2}'.format(i, avg_acc,avg_cost))
-            log_arr.append([avg_cost,avg_acc,i])    
+            log_arr.append([avg_cost,avg_acc,i])
             np.savetxt('history.txt', np.array(log_arr))
 
 
+def load_model(chk_dir,chk_num):
+    with tf.Session() as session:
 
-def main():    
-    tensorflow_implementation()
+        saver = tf.train.import_meta_graph(chk_dir+'bengio_run_test-'+str(chk_num)+'.meta')
+        saver.restore(session,tf.train.latest_checkpoint(chk_dir))
+        graph = tf.get_default_graph()
+        x = graph.get_tensor_by_name("x_indexes:0")
+        y_ = graph.get_tensor_by_name("y_pred:0")
+        y_actual = graph.get_tensor_by_name("y_actual:0")
+        perplex = graph.get_tensor_by_name("perplexity:0")
+        accuracy = graph.get_tensor_by_name("accuracy:0")
+        cost = graph.get_tensor_by_name("cost:0")
+        corp = Corpus()
+        x_batch, y_actual_batch = corp.get_batch()
+        feed_dict_train = {
+            x: x_batch,
+            y_actual: y_actual_batch,
+        }
+        print(session.run([accuracy,perplex,cost], feed_dict_train))
+
+def main():
+    # tensorflow_implementation()
     # history= np.loadtxt('history.txt')
 
     # import matplotlib.pyplot as plt
@@ -176,6 +197,8 @@ def main():
     # print(history[:,1])
     # plt.plot(history[:,1])
     # plt.show()
+
+    load_model('./model_chkpnts_20/',20)
 
 
 
